05dcc8e7e2a67fef728bf27d5f0041cbfdc28ce8
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-11-23 23:09:44 +0000

    Wed, Nov 23, 2022, 11:09 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,5 +1,20 @@
 hours, minutes = input("what time is it? ")
 
+def main():
+    ...
+
+
+def convert(time):
+    ...hours, minutes = time.split(":")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+hours, minutes = input("what time is it? ")
+
 if hours == "7":
     print("breakfast time")
 elif hours == "8" and minutes == "00":
