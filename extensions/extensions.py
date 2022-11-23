50cc179138b42e61842261762505fc0e1a48e943
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-11-23 21:33:13 +0000

    Wed, Nov 23, 2022, 9:33 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -6,3 +6,11 @@ if response.endswith(".jpg"):
     print("image/jpeg")
 if response.endswith(".jpeg"):
     print("image/jpeg")
+if response.endswith(".png"):
+    print("image/png")
+ if response.endswith(".pdf"):
+    print("application/pdf")
+if response.endswith(".txt"):
+    print("text/plain")
+if response.endswith(".zip"):
+    print("application/zip")
