493d4eb1e65914fe95d69391b3b69690e2539860
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-11-23 20:39:35 +0000

    Wed, Nov 23, 2022, 8:39 PM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,4 @@
-answer = input('What is the The Answer to the Great Question? ")
+answer = input("What is the The Answer to the Great Question? ")
 if answer == 42 | forty-two | forty two
     print("yes")
 else:
