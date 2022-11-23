e3b963e1628d862f4390bd5e546f30a4e57389e3
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-11-23 20:46:30 +0000

    Wed, Nov 23, 2022, 8:46 PM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,4 @@
-answer = input("What is the The Answer to the Great Question? ")
+answer = lower(input("What is the The Answer to the Great Question? "))
 if answer == "42" or answer == "forty-two" or answer == "forty two":
     print("yes")
 else:
