1315c80d2861b0f02ff225021eac49fa447ddf3e
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-11-23 21:34:24 +0000

    Wed, Nov 23, 2022, 9:34 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -8,9 +8,11 @@ if response.endswith(".jpeg"):
     print("image/jpeg")
 if response.endswith(".png"):
     print("image/png")
- if response.endswith(".pdf"):
+if response.endswith(".pdf"):
     print("application/pdf")
 if response.endswith(".txt"):
     print("text/plain")
 if response.endswith(".zip"):
     print("application/zip")
+else:
+    print("application/octet-stream")
