49267b1c97743bb3aa7a4d23276ef57409c39b07
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-11-23 23:17:32 +0000

    Wed, Nov 23, 2022, 11:17 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -3,20 +3,20 @@
 def main():
     time = input("what time is it? ")
 
-if hours == "7":
-    print("breakfast time")
-elif hours == "8" and minutes == "0":
-    print("breakfast time")
+    if hours == "7":
+        print("breakfast time")
+    elif hours == "8" and minutes == "0":
+        print("breakfast time")
 
-elif hours == "12":
-    print("lunch time")
-elif hours == "13" and minutes == "0":
-    print("lunch time")
+    elif hours == "12":
+        print("lunch time")
+    elif hours == "13" and minutes == "0":
+        print("lunch time")
 
-elif hours == "18":
-    print("dinner time")
-elif hours == "19" and minutes == "0":
-    print("dinner time")
+    elif hours == "18":
+        print("dinner time")
+    elif hours == "19" and minutes == "0":
+        print("dinner time")
 
 
 def convert(time):
