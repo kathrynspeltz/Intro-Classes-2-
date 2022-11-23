46e1a0d4253b0652a3628293a8d7e8b09b0ec043
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-11-23 20:56:16 +0000

    Wed, Nov 23, 2022, 8:56 PM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,4 @@
-answer = input("What is the The Answer to the Great Question? ").casefold()
+answer = input("What is the The Answer to the Great Question? ").casefold().strip()
 if answer == "42" or answer == "forty-two" or answer == "forty two":
     print("yes")
 else:
