b49dc2875c17628de5169f98a057874e89251387
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-11-23 22:38:31 +0000

    Wed, Nov 23, 2022, 10:38 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,4 +1,16 @@
 hours, minutes = int(input("what time is it? ")).split(":")
 
-if hours = 7:
-    print()
+if hours == 7:
+    print(breakfast)
+if hours == 8 and minutes == 00:
+    print(breakfast)
+
+elif hours == 12:
+    print(lunch)
+elif hours == 13 and minutes == 00:
+    print(lunch)
+
+if hours == 18:
+    print(breakfast)
+if hours == 19 and minutes == 00:
+    print(breakfast)
