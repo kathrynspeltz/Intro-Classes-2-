3dd635f6991fadc852d3b6fd942e62f23106c9c2
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-11-23 21:15:15 +0000

    Wed, Nov 23, 2022, 9:15 PM +00:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,6 +1,6 @@
 response = input("How would you like to be greeted? ").casefold().strip()
 
-if response.startswith == "hello":
+if response.startswith("hello"):
     print("$0")
 elif response.startswith("h"):
     print("$20")
