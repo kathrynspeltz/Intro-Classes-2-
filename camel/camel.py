a447d0f54d6a9826bef8c6e112b7ffd68561d9fe
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-01 23:44:54 +0000

    Thu, Dec 1, 2022, 11:44 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,14 +1,11 @@
 name = input("camelcase: ")
-print("snakecase:", newname)
-def newname(i)
-    for i in name:
-        if i.islower():
-            print(i, end="")
-        else:
-            print("_" + i.lower(), end="")
+print("snakecase:", end="" )
+for i in name:
+    if i.islower():
+        print(i, end="")
+    else:
+        print("_" + i.lower(), end="")
 print("\n")
-
-
 
 
 
