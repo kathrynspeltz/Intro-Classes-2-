fb514d6cc52eaead92ffca0634c4ad8b63dd5432
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-02 02:02:38 +0000

    Fri, Dec 2, 2022, 2:02 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,10 +1,10 @@
 print("Amount Due: 50")
+value = int(input("Insert Coin: "))
 
 cost = 50
 added = 0
 
 while added <= 50
-    value = int(input("Insert Coin: "))
         if value == 25 or value == 10 or value == 5:
             added += value
                 if added <= 50:
