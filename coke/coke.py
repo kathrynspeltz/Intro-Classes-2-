fb81bc2395e0270a7204d4f95d97e769ae6d9787
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-02 03:06:33 +0000

    Fri, Dec 2, 2022, 3:06 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -9,11 +9,11 @@ if value == 25 or value == 10 or value == 5:
     added += value
     while added < 50:
         print("Amount Due: ", cost - added)
-        break
+            break
 
 else:
         print("Amount Due: ", cost - added)
-        break
+            break
 
 if added > 50:
     print("Change Owed: ", (added - 50))
