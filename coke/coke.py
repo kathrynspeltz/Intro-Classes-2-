d53c14d39e19960f19676c42683b7b9ba056bd04
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-02 03:22:39 +0000

    Fri, Dec 2, 2022, 3:22 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -6,16 +6,11 @@ added = 0
 
 while added <= 50:
     value = int(input("Insert Coin: "))
-        if value == 25 or value == 10 or value == 5:
+    if value == 25 or value == 10 or value == 5:
             added += value
             if added >= 50:
-                 break
+                print("Change Owed: ", (added - 50))
             else:
                 print("Amount Due: ", 50 - added)
-        else:
-                print("Amount Due: ", 50 - added)
-                    break
-
-
-if added > 50:
-    print("Change Owed: ", (added - 50))
+    else:
+        print("Amount Due: ", 50 - added)
