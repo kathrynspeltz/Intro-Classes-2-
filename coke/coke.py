e34b2b8e4072f36651fdf213765c351071129745
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-02 00:56:03 +0000

    Fri, Dec 2, 2022, 12:56 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,5 +1,9 @@
 print("Amount Due: 50")
 value = int(input("Insert Coin: "))
 
-def value
-    if value == 
+def value:
+    if value == 25:
+    ljfzsd
+    elif value == 10:
+    jsdfl
+    elif value == 5:
