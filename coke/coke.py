bc575564201a5637a2cb0a5c9dfa20f355ed9dfa
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-02 02:00:48 +0000

    Fri, Dec 2, 2022, 2:00 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -8,6 +8,9 @@ while added <= 50
         if value == 25 or value == 10 or value == 5:
             added += value
                 if added <= 50:
-        elif:
+                    return(added)
+                else:
+                    print("Amount Due: ")
+        else:
             print("Amount Due:", added)
 
