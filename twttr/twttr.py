be646a59fc9c4e9ffd246f6413febda6fdba91a6
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-04 16:39:45 +0000

    Sun, Dec 4, 2022, 4:39 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,7 @@
 tweet = input("Input: ")
 print("Output: ", end="" )
 for c in tweet:
-    if c == a or c == e or c == i or c ==o or c==u
+    if c == a or c == e or c == i or c == o or c== u
         print(i, end="")
     else:
         print("_" + i.lower(), end="")
