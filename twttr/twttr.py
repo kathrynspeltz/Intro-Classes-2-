21604ee91b523cfa0b8485ea6f0f36e33ee2b8be
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-04 16:55:15 +0000

    Sun, Dec 4, 2022, 4:55 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,7 @@
 tweet = input("Input: ")
 print("Output: ", end="" )
 for c in tweet:
-    if c.re.search("[a , e , i , o , u]"):
+    if c.search([a , e , i , o , u]):
         print(c, end="")
     else:
         print("", end="")
