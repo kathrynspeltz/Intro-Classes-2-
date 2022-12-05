1b5112e5c73261f6856f93346728e33deccd7d10
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-05 00:31:58 +0000

    Mon, Dec 5, 2022, 12:31 AM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -8,11 +8,13 @@ def main():
 
 def is_valid(s):
     if plate:
-        is alpha and 
+        is alpha and
 
 # All vanity plates must start with at least two letters
 
 # vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters
+
+if len()
 
 # Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
 
