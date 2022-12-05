15920ed648552e9d18d4b162d5df9115a8cb8f9b
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-05 01:47:45 +0000

    Mon, Dec 5, 2022, 1:47 AM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,2 +1,17 @@
 fruit = input("Item: ")
-nutrition 
+nutrition = [
+    {"fruit": "apple", "calories": "130"},
+    {"fruit": "avacado", "calories": "50"},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+    {"fruit": "", "calories": ""},
+]
