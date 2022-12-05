a4834885965767c1483474688203365c847bfe76
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-05 01:37:08 +0000

    Mon, Dec 5, 2022, 1:37 AM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,7 +7,7 @@ def main():
 
 
 def is_valid(s):
-    if s[0,2].isalpha() and 2 <= len(s) <= 7 and s.isalnum() and s[0,1] != 0:
+    if s[0,2].isalpha() and 2 <= len(s) <= 7 and s.isalnum() and s.int([0,1]) != 0:
         return True
 
     else:
