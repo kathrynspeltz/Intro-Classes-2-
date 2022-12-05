1011ee7a3cfd787e4bea4afa2405aa0f96aa06aa
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-05 00:48:16 +0000

    Mon, Dec 5, 2022, 12:48 AM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -18,8 +18,7 @@ s.[0,2]isalpha()
 # Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
 
 # No periods, spaces, or punctuation marks are allowed
-
-
+s.isalnum()
 
 
 
