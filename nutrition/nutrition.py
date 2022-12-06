6cabab74959d3ca8a0b8872d3c14264250531c07
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-06 00:32:28 +0000

    Tue, Dec 6, 2022, 12:32 AM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -23,3 +23,12 @@ nutrition = [
 ]
 for fruit in nutrition:
     print(int(nutrition["calories"]))
+
+
+
+    dictionary = {'george' : 16, 'amber' : 19}
+search_age = raw_input("Provide age")
+for age in dictionary.values():
+    if age == search_age:
+        name = dictionary[age]
+        print name
