645b77b6f290f615417c2a358bfaa8c6ead6976d
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-06 01:57:33 +0000

    Tue, Dec 6, 2022, 1:57 AM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -3,9 +3,11 @@ nutrition = {"apple": 130, "avocado": 50, "banana": 110, "cantaloupe": 50, "grap
     "kiwifruit": 90, "lemon": 15, "lime": 20, "nectarine": 60, "orange": 80, "peach": 60, "pear": 100, "pineapple": 50,
     "plums": 70, "strawberries": 50, "sweet cherries": 100, "tangerine": 50, "watermelon": 80}
 
+def nutrition_calc():
+    for fruit in nutrition.values():
+        if fruit == food:
+            print("Calories:")
+        else:
+            print("oops")
 
-for fruit in nutrition.values():
-    if fruit == food:
-        print("Calories:")
-    else:
-        print("oops")
+nutrition_calc()
