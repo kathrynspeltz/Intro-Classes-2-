5e2fa35de955b0b2d4fde8c4d34cec994e34d25d
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-06 01:15:15 +0000

    Tue, Dec 6, 2022, 1:15 AM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,29 +1,7 @@
-food
-
-
- = input("Item: ")
-nutrition = [
-    {"fruit": "apple", "calories": "130"},
-    {"fruit": "avacado", "calories": "50"},
-    {"fruit": "banana", "calories": "110"},
-    {"fruit": "cantaloupe", "calories": "50"},
-    {"fruit": "grapefruit", "calories": "60"},
-    {"fruit": "grapes", "calories": "90"},
-    {"fruit": "honeydew melon", "calories": "50"},
-    {"fruit": "kiwifruit", "calories": "90"},
-    {"fruit": "lemon", "calories": "15"},
-    {"fruit": "lime", "calories": "20"},
-    {"fruit": "nectarine", "calories": "60"},
-    {"fruit": "orange", "calories": "80"},
-    {"fruit": "peach", "calories": "60"},
-    {"fruit": "pear", "calories": "100"},
-    {"fruit": "pineapple", "calories": "50"},
-    {"fruit": "plums", "calories": "70"},
-    {"fruit": "strawberries", "calories": "50"},
-    {"fruit": "sweet cherries", "calories": "100"},
-    {"fruit": "tangerine", "calories": "50"},
-    {"fruit": "watermelon", "calories": "80"},
-]
+food = input("Item: ")
+nutrition = {"apple": 130, "avacado": 50, "banana": 110, "cantaloupe": 50, "grapefruit": 60, "grapes": 90, "honeydew": 50,
+    "kiwifruit": 90, "lemon": 15, "lime": 20, "nectarine": 60, "orange": 80, "peach": 60, "pear": 100, "pineapple": 50,
+    "plums": 70, "strawberries": 50, "sweet cherries": 100, "tangerine": 50, "watermelon": 80}
 
 
 for fruit in nutrition.values():
