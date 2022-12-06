4ba5ed30a7a52d1779fb496b587a6c83d8319948
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2022-12-06 02:27:41 +0000

    Tue, Dec 6, 2022, 2:27 AM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -7,6 +7,6 @@ nutrition = {"apple": 130, "avocado": 50, "banana": 110, "cantaloupe": 50, "grap
 
 for fruit in nutrition:
     if fruit == food:
-        print(nutrition[key])
+        print(nutrition[fruit])
     else:
         print("oops")
