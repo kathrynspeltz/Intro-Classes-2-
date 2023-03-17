f44a311e6ecf1a22b26c927b0a212e51009fe42d
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-17 12:48:45 +0000

    Fri, Mar 17, 2023, 12:48 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,11 +1,8 @@
 
-
-def animal_crackers(animal):
-    wordlist = animal.split()
-    if wordlist [0][0] == wordlist [1][0]:
+def makes_twenty(a,b):
+    if a == 20 or b == 20 or a + b == 20:
         return True
     else:
         return False
 
-
-print(animal_crackers('Levelheaded zlama'))
+print(makes_twenty(10,10))
