63ab83089e26b621d75af3daf8ada9af08bea8e2
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-17 12:52:31 +0000

    Fri, Mar 17, 2023, 12:52 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,3 +1,9 @@
 
 def old_macdonald(name):
-    return name[0].upper()
+    for i in name:
+        if i == 0 or i == 3:
+            return i.upper()
+        else:
+            return i
+
+print(old_macdonald("macdonald"))
