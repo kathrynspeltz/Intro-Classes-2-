5c2753547ed199f380a2175633f3a5c6900d1303
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-18 20:28:11 +0000

    Sat, Mar 18, 2023, 8:28 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,9 +1,10 @@
 
-lista = [1, 3, 3]
+lista = [1, 4, 3]
 
-def has_33(listb):
-    for i in listb:
-        if i == 3 and i[i + 1] == 3:
+def has_33(nums):
+    for i in range(0, len(nums)-1):
+        if nums[i] == 3 and nums[i+1] == 3:
             return True
+    return False
 
 print(has_33(lista))
