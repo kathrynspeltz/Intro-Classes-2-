8bfbd4d89e885bda5afd6c06adb18bfe20d208fc
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-19 16:42:14 +0000

    Sun, Mar 19, 2023, 4:42 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,9 +1,13 @@
 
 def blackjack(a,b,c):
     blacknew = 0
-    if a + b + c =
+    if a + b + c <= 21:
+        return (a + b + c)
+    elif a == 11 or b == 11 or c == 11 and a + b + c > 21:
+        return ((a + b + c) - 10)
 
-print(blackjack(5,6,7))
+
+print(blackjack(5,6,11))
 
 #Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their
-sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
+#sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
