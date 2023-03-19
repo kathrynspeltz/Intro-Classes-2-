d8fc1a3ede8592c1ac3a1e1ce5642cbe00d4e5eb
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-19 16:07:09 +0000

    Sun, Mar 19, 2023, 4:07 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -5,6 +5,9 @@ def shopping(a,b):
     bill1 = 0
     for (a, b) in zip(price, discount):
         bill1 += (a * b)
-    return(bill1)
+    if bill1 > 20:
+        return "cannot afford"
+    else:
+        return "good to go"
 
 print(shopping(price, discount))
