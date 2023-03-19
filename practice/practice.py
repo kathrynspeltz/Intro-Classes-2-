367d8700d10592f37613e9e6c5bd8c02333e8af4
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-19 16:25:33 +0000

    Sun, Mar 19, 2023, 4:25 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,13 +1,4 @@
-price = [2, 5, 7]
-discount = [.5, .2, .3]
+name = "kathryn"
+age = 28
 
-def shopping(c,d):
-    bill1 = 0
-    for (a, b) in zip(c,d):
-        bill1 += (a * b)
-    if bill1 > 20:
-        return "cannot afford"
-    else:
-        return "good to go"
-
-print(shopping(price, discount))
+print(name, "is", age)
