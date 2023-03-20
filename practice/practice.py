4fbfd410a7d570ce8a43221afcd24d4159ea2481
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-20 12:21:53 +0000

    Mon, Mar 20, 2023, 12:21 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,12 +1,8 @@
 
-def summer_69(numbers):
-    for i in numbers:
-        if i == 6:
-            
-        else:
-            return i
+a = "my name is kathryn"
+b = "hello"
 
-summer_69([1, 3, 5])
+print(a,b)
 
-#Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9
-#(every 6 will be followed by at least one 9). Return 0 for no numbers.
+
+
