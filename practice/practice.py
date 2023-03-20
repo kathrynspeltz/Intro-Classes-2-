2d0e56584b10ce16be0484e4d5a20f3105256ff2
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-20 13:12:54 +0000

    Mon, Mar 20, 2023, 1:12 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -2,13 +2,7 @@
 
 #What is the largest prime factor of the number 600851475143
 
-def fibonacci(nums):
-    num1 = []
-    for i in range(nums):
-        if i%2 == 0:
-            num1 += i
-    return num1[-1]
 
 
-print(fibonacci(600851475143))
+print(fibonacci(list2))
 
