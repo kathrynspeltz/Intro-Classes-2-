17741506ddae417b2dc63c03c6eaf7ab9f4e19b2
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-20 00:13:07 +0000

    Mon, Mar 20, 2023, 12:13 AM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,13 +1,10 @@
 
-def blackjack(a,b,c):
-    blacknew = 0
-    if a + b + c <= 21:
-        return (a + b + c)
-    elif a == 11 or b == 11 or c == 11 and a + b + c > 21:
-        return ((a + b + c) - 10)
+def summer_69(numbers):
+    for i in numbers:
+        if i == 6:
+            
 
+summer_69([1, 3, 5])
 
-print(blackjack(5,6,11))
-
-#Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their
-#sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
+#Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9
+#(every 6 will be followed by at least one 9). Return 0 for no numbers.
