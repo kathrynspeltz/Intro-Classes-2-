579bfd5b9e024c2922e255ed5a3c380f4c4b9d6f
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-20 12:50:01 +0000

    Mon, Mar 20, 2023, 12:50 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -2,4 +2,8 @@
 #1, 2, 3, 5, 8, 13, 21, 34, 55, 89,
 #By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
+def fibonacci(nums):
+    list2 = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    for i in range(nums):
+        list2 = list2.append(i +  i[i-])
 
