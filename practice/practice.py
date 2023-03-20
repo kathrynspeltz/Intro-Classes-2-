cc74698af99ab64cb95085234fed22d7f6b90ea8
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-20 12:35:20 +0000

    Mon, Mar 20, 2023, 12:35 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,9 +1,9 @@
 def multiples(nums):
     value = 0
-    for i in nums:
+    for i in range(nums):
         if i%3 == 0 or i%5 == 0:
             value += i
-    return i
+    return value
 
 print(multiples(10))
 
