e3911d014da39c07747eaefcd3c1610c92182e9c
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-21 21:24:02 +0000

    Tue, Mar 21, 2023, 9:24 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,9 +1,11 @@
 
+list2 = [2, 4, 6, 8]
+
 def show_even_indices(nums):
     lista = []
     for i in nums:
         if i % 2 == 0:
-            lista.append(nums[i])
+            lista += i
     return lista
 
-print(show_even_indices([2, 4, 6, 8]))
+print(show_even_indices(list2))
