b5300d1f8277ff1887d469aca136f8099204c4ac
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-21 13:15:10 +0000

    Tue, Mar 21, 2023, 1:15 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,4 +1,9 @@
 
-list1 = [1, 2, 4, 2, 1, 4, 5]
-res = [*set(list1)]
-print(res)
+def vowels(words):
+    newlist = []
+    for n in words:
+        if n[0] == "i" or n[0] == "e" or n[0] == "o" or n[0] == "u":
+            newlist += n
+    return newlist
+
+print(vowels(["elephant", "hello", "octopus"]))
