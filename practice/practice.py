bc9ac067c170adbbf8b9db9909090a78883fce64
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-21 13:19:09 +0000

    Tue, Mar 21, 2023, 1:19 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -3,7 +3,7 @@ def vowels(words):
     newlist = ""
     for n in words:
         if n[0] == "i" or n[0] == "e" or n[0] == "o" or n[0] == "u":
-            newlist += n
+            newlist.append(n)
     return newlist
 
 print(vowels(["elephant", "hello", "octopus"]))
