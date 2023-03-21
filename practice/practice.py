9265f76745a161f1848d1a9d3424825883f36041
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-21 21:49:15 +0000

    Tue, Mar 21, 2023, 9:49 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -4,7 +4,7 @@ listc =['o', 'b', 'c']
 def replacementa(nums):
     for i in nums:
         if i == "A" or i == "a" or i == "e" or i == "E" or i == "I" or i == "i" or i == "o" or i == "O" or i == "U" or i == "u":
-            listc.replace(i, "*")
+            nums[i] = "*"
     return nums
 
 print(replacementa(listc))
