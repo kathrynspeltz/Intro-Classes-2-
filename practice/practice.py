f18728fd700a16b8340dfda5c3610eaa533dc0a3
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-21 21:45:18 +0000

    Tue, Mar 21, 2023, 9:45 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -2,9 +2,7 @@
 def replacementa(nums):
     for i in nums:
         if i == "A" or i == "a" or i == "e" or i == "E" or i == "I" or i == "i" or i == "o" or i == "O" or i == "U" or i == "u":
-            nums.replace("8)
-        else:
-            return i
+            nums.replace(i, "*")
     return nums
 
 print(replacementa(['o', 'b', 'c']))
