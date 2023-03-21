b22af83cf741b880a49b3466eded21bda472314f
kathrynspeltz <115676394+kathrynspeltz@users.noreply.github.com>
2023-03-21 21:17:49 +0000

    Tue, Mar 21, 2023, 9:17 PM +00:00

--- practice/practice.py
+++ practice/practice.py
@@ -1,17 +1,8 @@
 
-lista = [2, 4, 6, 8, 11, 3, 6]
-
-def oddeven(nums):
-    listeven = []
-    listodd = []
+def show_even_indices(nums):
+    lista = []
     for i in nums:
-        if i%2 == 0:
-            listeven.append(i)
-        else:
-            listodd.append(i)
-    if len(listeven) > len(listodd):
-        return "Even"
-    elif len(listeven) < len(listodd):
-        return "Odd"
+        if nums[i] % 2 == 0:
+            indices.append(i)
 
-print(oddeven(lista))
+    return indices
